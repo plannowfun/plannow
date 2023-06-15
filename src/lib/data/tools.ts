725a688ebf756d91a4f8bb45bf65b0ddfcff3803
plannow <254138142@qq.com>
2023-06-15 11:50:00 +0800

    修正时间转换的bug

--- src/lib/data/tools.ts
+++ src/lib/data/tools.ts
@@ -5,30 +5,30 @@ import weekOfYear from 'dayjs/plugin/weekOfYear'
 
 dayjs.extend(weekOfYear)
 export function getTime(date: number, view: string) {
-    const time = dayjs(String(date), 'YYYYMMDD')
+    const _date = dayjs(String(date), 'YYYYMMDD')
     if (view === 'day') {
         return String(date)
     } else if (view === 'week') {
-        return `${time.year()}${time.week()}`
+        return `${_date.year()}${_date.week()}`
     } else if (view === 'month') {
-        return `${time.year()}${time.month()}`
+        return `${_date.year()}${_date.month()}`
     } else if (view === 'year') {
-        return `${time.year()}`
+        return `${_date.year()}`
     } else {
         console.log(`the value of view is ${JSON.stringify(view)}`)
         throw new Error('unkown view value')
     }
 }
 export function getTimeTitle(date: number, view: string) {
-    const time = dayjs(String(date), 'YYYYMMDD')
+    const _date = dayjs(String(date), 'YYYYMMDD')
     if (view === 'day') {
-        return time.format('YYYY年M月D日');
+        return _date.format('YYYY年M月D日');
     } else if (view === 'week') {
-        return `${time.year()}年第${time.week() + 1}周`
+        return `${_date.year()}年第${_date.week()}周`
     } else if (view === 'month') {
-        return `${time.year()}年${time.month() + 1}月`
+        return `${_date.year()}年${_date.month() + 1}月`
     } else if (view === 'year') {
-        return `${time.year()}年`
+        return `${_date.year()}年`
     } else {
         console.log(`the value of view is ${JSON.stringify(view)}`)
         throw new Error('unkown view value')
@@ -39,17 +39,17 @@ export function getDate(view:string, time:string){
     if (view === 'day') {
         return parseInt(time)
     } else if (view === 'week') {
-        // 如果time是202301，那么就是2023年的第一周，返回2023年的第一周的第一天
+        // 如果time是20231，那么就是2023年的第一周，返回2023年的第一周的第一天
         // 如果time是202252，那么就是2022年的第52周，返回2022年的第52周的第一天
-        return dayjs(time, 'YYYYWW').week(parseInt(time.slice(4))).day(0).format('YYYYMMDD')
+        return dayjs(time.slice(0,4), 'YYYY').week(parseInt(time.slice(4))).day(0).format('YYYYMMDD')
     } else if (view === 'month') {
-        // 如果time是202301，那么就是2023年的第一月，返回2023年的第一月的第一天
-        // 如果time是202212，那么就是2022年的第12月，返回2022年的第12月的第一天
-        return dayjs(time, 'YYYYMM').month(parseInt(time.slice(4))).date(1).format('YYYYMMDD')
+        // 如果time是20230，那么就是2023年的第一月，返回2023年的第一月的第一天
+        // 如果time是202211，那么就是2022年的第12月，返回2022年的第12月的第一天
+        return dayjs(time, 'YYYYM').month(parseInt(time.slice(4))).format('YYYYMMDD')
     } else if (view === 'year') {
         // 如果time是2023，那么就是2023年，返回2023年的第一天
         // 如果time是2022，那么就是2022年，返回2022年的第一天
-        return dayjs(time, 'YYYY').year(parseInt(time)).dayOfYear(1).format('YYYYMMDD')
+        return dayjs(time, 'YYYY').year(parseInt(time)).format('YYYYMMDD')
     } else {
         console.log(`the value of view is ${JSON.stringify(view)}`)
         throw new Error('unkown view value')
